cde6ff4d7480a975a9086e1b8e881327bf5fb7c9
Matt Crowe <github.mpc@zym.me>
2017-02-16 23:01:35 +0000

    glue refactor

--- resources/js/glue.js
+++ resources/js/glue.js
@@ -1,16 +1,9 @@
-import blockIndex from './components/block/ctlr-index';
-import blockCreate from './components/block/ctlr-create';
-import blockEdit  from './components/block/ctlr-edit';
 import categoryIndex from './components/category/ctlr-index';
 import categoryCreate from './components/category/ctlr-create';
 import categoryEdit  from './components/category/ctlr-edit';
-import pageIndex from './components/page/ctlr-index';
-import pageCreate from './components/page/ctlr-create';
-import pageEdit  from './components/page/ctlr-edit';
 import tagIndex from './components/tag/ctlr-index';
 import tagCreate from './components/tag/ctlr-create';
 import tagEdit  from './components/tag/ctlr-edit';
-import store from 'belt/core/js/store/index';
 
 export default class BeltGlue {
 
@@ -22,22 +15,16 @@ export default class BeltGlue {
                 mode: 'history',
                 base: '/admin/belt/glue',
                 routes: [
-                    {path: '/blocks', component: blockIndex, canReuse: false, name: 'blockIndex'},
-                    {path: '/blocks/create', component: blockCreate, name: 'blockCreate'},
-                    {path: '/blocks/edit/:id', component: blockEdit, name: 'blockEdit'},
                     {path: '/categories', component: categoryIndex, canReuse: false, name: 'categoryIndex'},
                     {path: '/categories/create', component: categoryCreate, name: 'categoryCreate'},
                     {path: '/categories/edit/:id', component: categoryEdit, name: 'categoryEdit'},
-                    {path: '/pages', component: pageIndex, canReuse: false, name: 'pageIndex'},
-                    {path: '/pages/create', component: pageCreate, name: 'pageCreate'},
-                    {path: '/pages/edit/:id', component: pageEdit, name: 'pageEdit'},
                     {path: '/tags', component: tagIndex, canReuse: false, name: 'tagIndex'},
                     {path: '/tags/create', component: tagCreate, name: 'tagCreate'},
                     {path: '/tags/edit/:id', component: tagEdit, name: 'tagEdit'},
                 ]
             });
 
-            const app = new Vue({router, store}).$mount('#belt-glue');
+            const app = new Vue({router}).$mount('#belt-glue');
         }
     }
 
