e474b9e2fb76877de2b95f0ec0bde8d6dd0d1b94
Matt Crowe <github.mpc@zym.me>
2017-03-08 23:24:38 +0000

    taggable scope

--- src/Behaviors/Taggable.php
+++ src/Behaviors/Taggable.php
@@ -11,4 +11,21 @@ trait Taggable
         return $this->morphToMany(Tag::class, 'taggable');
     }
 
+    /**
+     * Return items associated with the given tag
+     *
+     * @param $query
+     * @param $tag
+     * @return mixed
+     */
+    public function scopeHasTag($query, $tag)
+    {
+        $query->whereHas('tags', function ($subQB) use ($tag) {
+            $column = is_numeric($tag) ? 'id' : 'slug';
+            $subQB->where($column, $tag);
+        });
+
+        return $query;
+    }
+
 }
