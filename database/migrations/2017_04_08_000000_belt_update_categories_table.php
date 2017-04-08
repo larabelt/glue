de097df244d7de4201ab0f780f5ed87b9b943067
rob.lasota <rob.lasota@fahlgren.com>
2017-04-08 10:14:06 -0400

    adding is_active to categories table

---
+++ database/migrations/2017_04_08_000000_belt_update_categories_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BeltUpdateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->boolean('is_active')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
