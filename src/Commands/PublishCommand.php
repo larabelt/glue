5875b9f9b663f8720443acb4d89ac2914a7f57ad
Matt Crowe <github.mpc@zym.me>
2017-06-16 18:16:51 +0000

    Revert "publish --config flag"
    This reverts commit a2ae74af285c23eaa59773e531559e25b90dbfa5.

--- src/Commands/PublishCommand.php
+++ src/Commands/PublishCommand.php
@@ -12,7 +12,7 @@ class PublishCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'belt-glue:publish {action=publish} {--force} {--include=} {--exclude=} {--config}';
+    protected $signature = 'belt-glue:publish {action=publish} {--force} {--include=} {--exclude=}';
 
     /**
      * The console command description.
