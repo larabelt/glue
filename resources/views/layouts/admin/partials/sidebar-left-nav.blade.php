1b665b9b637a6c52c8a4df8a4118a611efb5a948
Matt Crowe <github.mpc@zym.me>
2017-08-18 21:01:01 +0000

    extend teams functionality init

--- resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
+++ resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
@@ -1,9 +1,11 @@
-<li class="treeview">
-    <a href="#">
-        <i class="fa fa-tint"></i> <span>Glue Admin</span> <i class="fa fa-angle-left pull-right"></i>
-    </a>
-    <ul class="treeview-menu">
-        <li><a href="/admin/belt/glue/categories"><i class="fa fa-sitemap"></i> <span>Categories</span></a></li>
-        <li><a href="/admin/belt/glue/tags"><i class="fa fa-tags"></i> <span>Tags</span></a></li>
-    </ul>
-</li>
+@if(!$team)
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-tint"></i> <span>Glue Admin</span> <i class="fa fa-angle-left pull-right"></i>
+        </a>
+        <ul class="treeview-menu">
+            <li><a href="/admin/belt/glue/categories"><i class="fa fa-sitemap"></i> <span>Categories</span></a></li>
+            <li><a href="/admin/belt/glue/tags"><i class="fa fa-tags"></i> <span>Tags</span></a></li>
+        </ul>
+    </li>
+@endif
