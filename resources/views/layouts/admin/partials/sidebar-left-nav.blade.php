7f1bb3a6bb28aecfce55a3ca6bf73b9ecf66eebb
Matt Crowe <github.mpc@zym.me>
2018-03-19 14:39:01 +0000

    sidebar nav ids

--- resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
+++ resources/views/layouts/admin/partials/sidebar-left-nav.blade.php
@@ -4,16 +4,16 @@
 @endphp
 
 @if($can['categories'] || $can['tags'])
-    <li class="treeview">
+    <li id="glue-admin-sidebar-left" class="treeview">
         <a href="#">
             <i class="fa fa-tint"></i> <span>Glue Admin</span> <i class="fa fa-angle-left pull-right"></i>
         </a>
         <ul class="treeview-menu">
             @if($can['categories'])
-                <li><a href="/admin/belt/glue/categories"><i class="fa fa-sitemap"></i> <span>Categories</span></a></li>
+                <li id="glue-admin-sidebar-left-categories"><a href="/admin/belt/glue/categories"><i class="fa fa-sitemap"></i> <span>Categories</span></a></li>
             @endif
             @if($can['tags'])
-                <li><a href="/admin/belt/glue/tags"><i class="fa fa-tags"></i> <span>Tags</span></a></li>
+                <li id="glue-admin-sidebar-left-tags"><a href="/admin/belt/glue/tags"><i class="fa fa-tags"></i> <span>Tags</span></a></li>
             @endif
         </ul>
     </li>
